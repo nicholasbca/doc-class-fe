cc81e3af9081fc13a042140804f770ee05bd0227
Nicholas BCA <nicholas_setyabudi@bca.co.id>
2023-02-15 10:31:21 +0700

    Updates

--- precache-manifest.6d484b641e1e639fa7c8f0465f80575a.js
+++ precache-manifest.6d484b641e1e639fa7c8f0465f80575a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "155de7ddcd684ccaff6d0c5b164afde2",
+    "revision": "2f5e632c0e9503d8cc85c5a695c4437f",
     "url": "/doc-class-fe/index.html"
   },
   {
-    "revision": "ab31391ae02f7b1ce8ad",
+    "revision": "618bbc8925948d44634f",
     "url": "/doc-class-fe/static/css/2.d4bd74a8.chunk.css"
   },
   {
-    "revision": "80597a74461328f4c73d",
+    "revision": "a465359ea66708e1b29a",
     "url": "/doc-class-fe/static/css/main.fc79f4fa.chunk.css"
   },
   {
-    "revision": "ab31391ae02f7b1ce8ad",
-    "url": "/doc-class-fe/static/js/2.e892ed81.chunk.js"
+    "revision": "618bbc8925948d44634f",
+    "url": "/doc-class-fe/static/js/2.a0b90b4e.chunk.js"
   },
   {
     "revision": "8041207c6ef8ff1fb65254e32bc18cb7",
-    "url": "/doc-class-fe/static/js/2.e892ed81.chunk.js.LICENSE.txt"
+    "url": "/doc-class-fe/static/js/2.a0b90b4e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "80597a74461328f4c73d",
-    "url": "/doc-class-fe/static/js/main.487cea60.chunk.js"
+    "revision": "a465359ea66708e1b29a",
+    "url": "/doc-class-fe/static/js/main.9b4495cd.chunk.js"
   },
   {
     "revision": "e0f56da4016ef038b470",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/doc-class-fe/precache-manifest.22f35a03c210185e02a44b7b30b82cd1.js"
+  "/doc-class-fe/precache-manifest.6d484b641e1e639fa7c8f0465f80575a.js"
 );
 
 self.addEventListener('message', (event) => {
